b54f12788cee6e920ccbe497dc774a7dcf0b1cf1
codegnomic <codegnomic@proton.me>
2022-09-18 10:31:51 -0400

    made nav conditional

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,7 +1,22 @@
+<script>
+    import { state } from "$lib/stores"
+
+    const handleSignOut = async () => {
+        try {
+            await state.signOut()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+</script>
+
 <nav>
-    <a href="/">Home</a>
-    <a href="/login">Login</a>
-    <a href="/register">Register</a>
-    <a href="/profile">Profile</a>
-    <a href="/chat">Chat</a>
+    <a href="/"><button>Home</button></a>
+    {#if $state.user}
+        <a href="/profile"><button>Profile</button></a>
+        <button on:click={handleSignOut}>Sign Out</button>
+    {:else}
+        <a href="/login"><button>Login</button></a>
+        <a href="/register"><button>Register</button></a>
+    {/if}
 </nav>
