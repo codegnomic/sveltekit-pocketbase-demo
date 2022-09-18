df09d554df6508406a49b9010f284d14cf52ff0d
codegnomic <codegnomic@proton.me>
2022-09-18 10:30:32 -0400

    added signout method

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,28 +1,32 @@
-import {client} from '$lib/db'
-import {writable} from 'svelte/store'
+import { client } from "$lib/db"
+import { writable } from "svelte/store"
 
-function createStores () {
-    const {subscribe, set, update} = writable({user: null, alert: null})
+function createStores() {
+    const { subscribe, set, update } = writable({ user: null, alert: null })
 
     return {
         subscribe,
         login: async (email, password) => {
             let userData = await client.users.authViaEmail(email, password)
-            state.init(userData) 
+            state.init(userData)
         },
         register: async (email, password, passwordConfirm) => {
-            await client.users.create({email, password, passwordConfirm})
+            await client.users.create({ email, password, passwordConfirm })
         },
-        registerAdmin: async(email, password, passwordConfirm) => {
-            await client.admins.create({email, password, passwordConfirm})
+        registerAdmin: async (email, password, passwordConfirm) => {
+            await client.admins.create({ email, password, passwordConfirm })
         },
         refresh: async () => {
             let userData = await client.users.refresh()
             state.init(userData)
         },
+        signOut: async () => {
+            await client.authStore.clear()
+            state.init()
+        },
         init: async (user = null) => {
-            return set({user, alert: null})
-        }, 
+            return set({ user, alert: null })
+        },
     }
 }
 
