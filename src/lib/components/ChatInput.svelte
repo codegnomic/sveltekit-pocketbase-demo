a92b98334beab700bb9cbe308f3c146c202c813b
codegnomic <codegnomic@proton.me>
2022-09-18 10:31:13 -0400

    moved chatinput to it's own component file

---
+++ src/lib/components/ChatInput.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    import { createPicker } from "picmo"
+    import { createPopperActions } from "svelte-popperjs"
+    import { onMount } from "svelte"
+    import { state } from "$lib/stores"
+    import { client } from "$lib/db"
+
+    $: console.log($state.user.user.profile.id)
+
+    let input = ""
+    let showPicker = false
+    let caretPosition = 0
+
+    onMount(() => {
+        const rootElement = document.querySelector("#pickerContainer")
+        const picker = createPicker({ rootElement })
+
+        picker.addEventListener("emoji:select", (event) => {
+            input =
+                input.slice(0, caretPosition) +
+                event.emoji +
+                input.slice(caretPosition)
+        })
+    })
+
+    const [popperRef, popperContent] = createPopperActions({
+        placement: "top",
+        strategy: "fixed",
+    })
+
+    const extraOpts = {
+        modifiers: [{ name: "offset", options: { offset: [-40, 0] } }],
+    }
+
+    const handleCaretPosition = (e) => {
+        caretPosition = e.target.selectionStart
+    }
+
+    const handleSubmit = async () => {
+        if (input == "") return
+        try {
+            await client.records.create("posts", {
+                user: $state.user.user.profile.id,
+                body: input,
+            })
+            input = ""
+        } catch (error) {
+            console.log(error)
+        }
+    }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <input
+        type="text"
+        bind:value={input}
+        on:keyup={handleCaretPosition}
+        on:click={handleCaretPosition}
+    />
+    <span
+        use:popperRef
+        on:click={() => {
+            showPicker = !showPicker
+        }}>😃</span
+    >
+    <button type="submit">Send</button>
+</form>
+
+<span use:popperContent={extraOpts} hidden={!showPicker} id="pickerContainer" />
--- src/lib/components/Chatbox.svelte
+++ src/lib/components/Chatbox.svelte
@@ -1,94 +1,42 @@
 <script lang="ts">
-  import { client } from "$lib/db"
-  import { onMount } from "svelte"
-  import { state } from "$lib/stores"
-  import { createPicker } from "picmo"
-  import { createPopperActions } from "svelte-popperjs"
+    import { client } from "$lib/db"
+    import { onMount } from "svelte"
+    import ChatInput from "./ChatInput.svelte"
 
-  export let posts
-  let input = ""
-  let showPicker = false
-  let caretPosition
+    export let posts
 
-  const [popperRef, popperContent] = createPopperActions({
-    placement: "top",
-    strategy: "fixed",
-  })
-
-  const extraOpts = {
-    modifiers: [{ name: "offset", options: { offset: [-40, 0] } }],
-  }
-
-  const togglePicker = () => {
-    showPicker = !showPicker
-  }
-
-  const handleSubmit = async () => {
-    if (input == "") return
-    try {
-      await client.records.create("posts", {
-        user: $state.user.user.id,
-        body: input,
-      })
-      input = ""
-    } catch (error) {
-      console.log(error)
+    async function fetchPosts() {
+        const postsData = await client.records.getList("posts", 1, 100, {
+            expand: "user",
+        })
+        posts = postsData.items
     }
-  }
 
-  const handleCaretPosition = (e) => {
-    caretPosition = e.target.selectionStart - 1
-  }
-
-  async function fetchPosts() {
-    const postsData = await client.records.getList("posts", 1, 100)
-    posts = postsData.items
-  }
-
-  onMount(() => {
-    client.realtime.subscribe("posts", function (e) {
-      fetchPosts()
+    onMount(() => {
+        client.realtime.subscribe("posts", function (e) {
+            fetchPosts()
+        })
     })
-
-    const rootElement = document.querySelector("#pickerContainer")
-    const picker = createPicker({ rootElement })
-
-    picker.addEventListener("emoji:select", (event) => {
-      input =
-        input.slice(0, caretPosition) + event.emoji + input.slice(caretPosition)
-    })
-  })
 </script>
 
 <div class="chatbox">
-  {#each posts as post}
-    <div>
-      <span>{post.user}: </span>
-      <p class="post-body">{post.body}</p>
-    </div>
-  {/each}
+    {#each posts as post}
+        <div>
+            <span>{post["@expand"].user.name}: </span>
+            <p class="post-body">{post.body}</p>
+        </div>
+    {/each}
 </div>
 
-<form on:submit|preventDefault={handleSubmit}>
-  <input
-    type="text"
-    bind:value={input}
-    bind:this={caretPosition}
-    on:keyup={handleCaretPosition}
-  />
-  <span use:popperRef on:click={togglePicker}>😃</span>
-  <button type="submit">Send</button>
-</form>
-
-<span use:popperContent={extraOpts} hidden={!showPicker} id="pickerContainer" />
+<ChatInput />
 
 <style>
-  .post-body {
-    display: inline;
-  }
-  .chatbox {
-    border: 1px solid black;
-    overflow: scroll;
-    height: 90vh;
-  }
+    .post-body {
+        display: inline;
+    }
+    .chatbox {
+        border: 1px solid black;
+        overflow: scroll;
+        height: 90vh;
+    }
 </style>
