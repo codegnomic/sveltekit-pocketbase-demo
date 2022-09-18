ccb1e4288b250d1c745b79841631f1855962095b
codegnomic <codegnomic@proton.me>
2022-09-18 10:32:07 -0400

    only display chat when logged in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
 <script>
     import Chatbox from "$lib/components/Chatbox.svelte"
+    import { state } from "$lib/stores"
 
     export let data
 </script>
 
-<Chatbox posts={data.posts.items} />
+{#if $state.user}
+    <Chatbox posts={data.posts.items} />
+{:else}
+    <p>
+        You need to be registered to see the chat, please <a href="/login"
+            >login</a
+        >
+    </p>
+{/if}
