d28451bc70ddde28ccea2b8105814b04f1ed19e3
Code Gnomic <codegnomic@proton.me>
2022-09-18 22:10:19 -0400

    added error handling, profile update upon register

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,32 +2,52 @@ import { client } from "$lib/db"
 import { writable } from "svelte/store"
 
 function createStores() {
-    const { subscribe, set, update } = writable({ user: null, alert: null })
+  const { subscribe, set, update } = writable({ user: null, alert: null })
 
-    return {
-        subscribe,
-        login: async (email, password) => {
-            let userData = await client.users.authViaEmail(email, password)
-            state.init(userData)
-        },
-        register: async (email, password, passwordConfirm) => {
-            await client.users.create({ email, password, passwordConfirm })
-        },
-        registerAdmin: async (email, password, passwordConfirm) => {
-            await client.admins.create({ email, password, passwordConfirm })
-        },
-        refresh: async () => {
-            let userData = await client.users.refresh()
-            state.init(userData)
-        },
-        signOut: async () => {
-            await client.authStore.clear()
-            state.init()
-        },
-        init: async (user = null) => {
-            return set({ user, alert: null })
-        },
-    }
+  return {
+    subscribe,
+    login: async (email, password) => {
+      try {
+        let userData = await client.users.authViaEmail(email, password)
+        state.init(userData)
+      } catch (error) {
+        return set({ user: null, alert: error.message })
+      }
+    },
+    register: async ({ email, password, passwordConfirm, username }) => {
+      try {
+        let user = await client.users.create({
+          email,
+          password,
+          passwordConfirm,
+        })
+        await client.users.authViaEmail(email, password)
+        await client.records.update("profiles", user.profile.id, {
+          name: username,
+        })
+        state.refresh()
+      } catch (error) {
+        return set({ user: null, alert: error.message })
+      }
+    },
+    registerAdmin: async (email, password, passwordConfirm, username) => {
+      await client.admins.create({ email, password, passwordConfirm })
+    },
+    refresh: async () => {
+      let userData = await client.users.refresh()
+      state.init(userData)
+    },
+    signOut: async () => {
+      await client.authStore.clear()
+      state.init()
+    },
+    clearAlert: () => {
+      return set({ user: null, alert: null })
+    },
+    init: async (user = null) => {
+      return set({ user, alert: null })
+    },
+  }
 }
 
 export const state = createStores()
