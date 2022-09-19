3a983f80c50ea4e4de3fa589e8287830953822f9
Code Gnomic <codegnomic@proton.me>
2022-09-18 22:10:44 -0400

    added auto scroll to chat window

--- src/lib/components/Chatbox.svelte
+++ src/lib/components/Chatbox.svelte
@@ -1,42 +1,48 @@
 <script lang="ts">
-    import { client } from "$lib/db"
-    import { onMount } from "svelte"
-    import ChatInput from "./ChatInput.svelte"
+  import { client } from "$lib/db"
+  import { onMount, afterUpdate } from "svelte"
+  import ChatInput from "./ChatInput.svelte"
 
-    export let posts
+  export let posts
+  let chatbox
 
-    async function fetchPosts() {
-        const postsData = await client.records.getList("posts", 1, 100, {
-            expand: "user",
-        })
-        posts = postsData.items
-    }
-
-    onMount(() => {
-        client.realtime.subscribe("posts", function (e) {
-            fetchPosts()
-        })
+  async function fetchPosts() {
+    const postsData = await client.records.getList("posts", 1, 100, {
+      expand: "user",
     })
+    posts = postsData.items
+  }
+
+  onMount(() => {
+    client.realtime.subscribe("posts", function (e) {
+      fetchPosts()
+    })
+    // chatbox.scrollTop = chatbox.scrollHeight
+  })
+
+  afterUpdate(() => {
+    chatbox.scrollTop = chatbox.scrollHeight
+  })
 </script>
 
-<div class="chatbox">
-    {#each posts as post}
-        <div>
-            <span>{post["@expand"].user.name}: </span>
-            <p class="post-body">{post.body}</p>
-        </div>
-    {/each}
+<div class="chatbox" bind:this={chatbox}>
+  {#each posts as post}
+    <div>
+      <span>{post["@expand"].user.name}: </span>
+      <p class="post-body">{post.body}</p>
+    </div>
+  {/each}
 </div>
 
 <ChatInput />
 
 <style>
-    .post-body {
-        display: inline;
-    }
-    .chatbox {
-        border: 1px solid black;
-        overflow: scroll;
-        height: 90vh;
-    }
+  .post-body {
+    display: inline;
+  }
+  .chatbox {
+    border: 1px solid black;
+    overflow: scroll;
+    height: 90vh;
+  }
 </style>
