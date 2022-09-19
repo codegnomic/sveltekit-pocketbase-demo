063caf98f594f47b026ac8e320fe876cc740037e
Code Gnomic <codegnomic@proton.me>
2022-09-18 22:11:38 -0400

    clear alert upon page refresh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-    import Nav from "$lib/components/Nav.svelte"
+  import { afterNavigate } from "$app/navigation"
+  import Nav from "$lib/components/Nav.svelte"
+  import { state } from "$lib/stores"
+
+  afterNavigate(() => {
+    if (!$state.user) {
+      state.clearAlert()
+    }
+  })
 </script>
 
 <Nav />
