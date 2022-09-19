f878748708a2224b0a917e45315f0321f071633d
Code Gnomic <codegnomic@proton.me>
2022-09-18 22:16:50 -0400

    added labels, page redirect,  and username field

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,26 +1,50 @@
 <script lang="ts">
-    import { state } from "$lib/stores"
-    let email: String
-    let password: String
-    let passwordConfirm: String
+  import { goto } from "$app/navigation"
+  import { state } from "$lib/stores"
+  let email: String
+  let password: String
+  let passwordConfirm: String
+  let username: String
 
-    const handleSubmit = async () => {
-        try {
-            let res = await state.register(email, password, passwordConfirm)
-            console.log(res)
-        } catch (error) {
-            console.log(error)
-        }
+  const handleSubmit = async () => {
+    let payload = {
+      email,
+      password,
+      passwordConfirm,
+      username,
     }
+    await state.register(payload)
+  }
+
+  $: if ($state.user) goto("/")
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
-    <input name="email" type="email" bind:value={email} />
-    <input name="password" type="password" bind:value={password} />
+  <div>
+    <label for="email">Email:</label>
+    <input name="email" type="email" bind:value={email} required />
+  </div>
+  <div>
+    <label for="password">Password:</label>
+    <input name="password" type="password" bind:value={password} required />
+  </div>
+  <div>
+    <label for="passwordConfirm">Confirm password:</label>
     <input
-        name="passwordConfirm"
-        type="password"
-        bind:value={passwordConfirm}
+      name="passwordConfirm"
+      type="password"
+      bind:value={passwordConfirm}
+      required
     />
-    <button type="submit">Register</button>
+  </div>
+  <div>
+    <label for="username">Choose a username:</label>
+    <input type="text" name="username" bind:value={username} required />
+  </div>
+
+  <button type="submit">Register</button>
 </form>
+
+{#if $state.alert}
+  <span>{$state.alert}</span>
+{/if}
