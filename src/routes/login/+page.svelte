f3a0d4cec20d54bd3863555ce0d90c9a599b790b
Code Gnomic <codegnomic@proton.me>
2022-09-18 22:15:53 -0400

    added form labels, username fields, and redirect

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
-    import { state } from "$lib/stores"
-    let email: String
-    let password: String
+  import { goto } from "$app/navigation"
+  import { state } from "$lib/stores"
+  let email: String
+  let password: String
 
-    const handleSubmit = async () => {
-        try {
-            await state.login(email, password)
-        } catch (error) {
-            console.log(error)
-        }
-    }
+  const handleSubmit = async () => {
+    await state.login(email, password)
+  }
+
+  $: if ($state.user) goto("/")
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
-    <input type="email" name="email" bind:value={email} />
-    <input type="password" name="password" bind:value={password} />
-    <button type="submit">Login</button>
+  <div>
+    <label for="email">Email:</label><input
+      type="email"
+      name="email"
+      bind:value={email}
+    />
+  </div>
+  <div>
+    <label for="password">Password:</label><input
+      type="password"
+      name="password"
+      bind:value={password}
+    />
+  </div>
+  <button type="submit">Login</button>
 </form>
+{#if $state.alert}
+  <span>{$state.alert}</span>
+{/if}
